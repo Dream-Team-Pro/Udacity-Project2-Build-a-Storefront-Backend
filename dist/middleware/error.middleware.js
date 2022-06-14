8549a26a387dfdaffd2fd6cf2d951b9f9558d4aa
Dream-Team-Pro <mz1320021@yahoo.com>
2022-06-14 23:07:33 +0300

    ignore node-dist

--- dist/middleware/error.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorMiddleware = (error, req, res, next) => {
-    const status = error.status || 500;
-    const message = error.message || 'oops, something wrong';
-    res.status(status).json({ status, message });
-};
-exports.default = errorMiddleware;
